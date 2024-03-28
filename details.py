db6fb26bbc41bd7e93544be8e4b3293d4d9775a4
RMS-Iqbal <158320774+RMS-Iqbal@users.noreply.github.com>
2024-03-28 21:30:50 +0000

    Add files via upload

---
+++ details.py
@@ -0,0 +1,18 @@
+# Ask the user's name, age, house number and street name
+
+# Create variables for name, age, house number and street name
+
+user_name = input (" What is your full name? ")
+
+user_age = input (" How old are you? ")
+
+user_house_number = input (" What is your house number? ")
+
+user_street = input (" What is the name of your street? ")
+
+# Print out all the information collected in a single sentence.
+
+print (f" This is {user_name} . He is {user_age} years old and lives at house number {user_house_number} on {user_street}.")
+
+# End of task
+
